e51ac0a2236f19924b63bfe72bda02dbe4840f4f
Kevin Hsieh <kevin899@gmail.com>
2018-02-14 13:15:37 -0800

    Improve user interface and robustness

--- auto-mfa.js
+++ auto-mfa.js
@@ -8,22 +8,22 @@ Kevin Hsieh
 
 setTimeout(() => {
 	// Add "Enter Passcodes" button.
-	document.getElementsByClassName("base-body")[0].children[0].innerHTML +=
-		"<input type='button' id='enter-passcodes' value='Enter Passcodes' />";
-	document.getElementById("enter-passcodes").onclick =
-		() => getPasscodes(true);
+	document.getElementById("login-form").innerHTML +=
+		"<input type='button' id='enter-passcodes' value='Enter Passcodes' />" +
+		"<p id='autoauth-message' align='center'></div>";
+	document.getElementById("enter-passcodes").onclick = () => autoFill(true);
 
 	// Page interaction.
 	let authButtons = document.getElementsByClassName("positive auth-button");
 	authButtons[authButtons.length - 1].click();
 	document.getElementsByName("passcode")[0].disabled = true;
-	getPasscodes();
+	autoFill();
 }, 100);
 
 // Retrieves and fills in the passcode.
 // resetPasscodes: Displays a prompt to enter and store new passcodes.
 // ignoreMissingNext: Inserts the first passcode if next can't be found.
-function getPasscodes(resetPasscodes = false, ignoreMissingNext = false) {
+function autoFill(resetPasscodes = false, ignoreMissingNext = false) {
 	browser.storage.local.get().then(item => {
 		let passcodes =
 			item.hasOwnProperty("passcodes") ? item["passcodes"] : null;
@@ -38,25 +38,33 @@ function getPasscodes(resetPasscodes = false, ignoreMissingNext = false) {
 			ignoreMissingNext ? 0 : -1;
 			// The index of the passcode we need, or -1 if unknown.
 		if (resetPasscodes) {
-			setPasscodes(passcodes ? passcodes : []);
+			setPasscodes();
 		}
 		else if (passcodes && next_i > lastUsed) {
 			document.getElementsByName("passcode")[0].value = passcodes[next_i];
-			document.getElementsByClassName("auth-button positive")[2].click();
 			setLastUsed(next_i);
+			document.getElementsByClassName("auth-button positive")[2].click();
+		}
+		else {
+			document.getElementById("autoauth-message").innerHTML =
+				"Please enter new passcodes.";
 		}
 	}, error => console.log("AutoAuth: Storage error!"));
 }
 
 // Displays a prompt to enter and store new passcodes.
-// currentPasscodes.join(" ") serves as the default value of the prompt.
 function setPasscodes(currentPasscodes) {
-	let passcodes = window.prompt("AutoAuth: Enter your next ten passcodes" +
-		" separated by spaces:", currentPasscodes.join(" ")).split(" ");
+	let passcodes = window.prompt("Enter the ten passcodes from your most" +
+		" recent MFA SMS message (separated by spaces):", "").split(" ");
+	if (passcodes.length != 10) {
+		document.getElementById("autoauth-message").innerHTML =
+			"Invalid entry. Please try again.";
+		return;
+	}
 	browser.storage.local.set({
 		"passcodes": passcodes,
 		"lastUsed": -1
-	}).then(() => getPasscodes(false, true),
+	}).then(() => autoFill(false, true),
 		error => console.log("AutoAuth: Storage error!"));
 }
 
